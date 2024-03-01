160dbe42fe7a8139e218789edcb68976bf854593
7Luke7 <pro.chikvaidze@gmail.com>
2024-03-02 01:05:24 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const sess = session({
         mongoUrl: process.env.DATABASE_URL,
     }),
     cookie: {   
-        secure: false,
+        secure: true,
     }
 })
 const origin = process.env.URL
